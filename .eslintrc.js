51cfcec85ae69b7b2749aec141cf19bc484de9c9
Jeka <buterfly0013@mail.ru>
2024-02-17 23:45:18 +0300

    Построил базовую структуру, настроил проект, сделал компонент Header

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    // 'react/no-unknown-property': ['error', { ignore: ['tsx', 'jsx'] }],
+    'no-var': 'error',
+    'prefer-const': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/App.scss';
+import Header from '../components/Header';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
